a7ed48e40f5f177f89a806ff04901b539f8aba34
Jacob <jcbcn@users.noreply.github.com>
2019-01-09 22:13:43 +0000

    Update deploy.cmd

--- deploy.cmd
+++ deploy.cmd
@@ -109,7 +109,7 @@ echo Handling Angular build
 :: 4. Build ng app
 IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
   pushd "%DEPLOYMENT_TARGET%"
-  call :ExecuteCmd "!NODE_EXE!" ./node_modules/@angular/cli/bin/ng build --prod --configuration=prod --aot
+  call :ExecuteCmd "!NODE_EXE!" ./node_modules/@angular/cli/bin/ng build --prod
   IF !ERRORLEVEL! NEQ 0 goto error
   :: the next line is optional to fix 404 error see section #8
   call :ExecuteCmd cp "%DEPLOYMENT_TARGET%"/web.config "%DEPLOYMENT_TARGET%"/dist/
