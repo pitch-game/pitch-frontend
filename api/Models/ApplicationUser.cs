f16575dcb87d24ee05289bf6a4c66d2dc625f7cf
Jacob <jcbcn@users.noreply.github.com>
2019-01-13 20:28:38 +0000

    openiddict

---
+++ api/Models/ApplicationUser.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace api.Models
+{
+  public class ApplicationUser : IdentityUser
+  {
+  }
+}
--- api/Startup.cs
+++ api/Startup.cs
@@ -2,14 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using api.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 
 namespace api
 {
@@ -26,6 +31,62 @@ namespace api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options =>
+            {
+                options.Authority = "https://localhost:44357/";
+                options.Audience = "pitch-api";
+                options.RequireHttpsMetadata = false;
+                options.IncludeErrorDetails = true;
+            });
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+               {
+                   // Configure the context to use Microsoft SQL Server.
+                   options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+
+                   // Register the entity sets needed by OpenIddict.
+                   // Note: use the generic overload if you need
+                   // to replace the default OpenIddict entities.
+                   options.UseOpenIddict();
+               });
+
+            // Register the Identity services.
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
+            // Register the OpenIddict services.
+            services.AddOpenIddict()
+                .AddCore(options =>
+                {
+                    // Configure OpenIddict to use the Entity Framework Core stores and entities.
+                    options.UseEntityFrameworkCore().UseDbContext<ApplicationDbContext>();
+                })
+
+                .AddServer(options =>
+                {
+                    // Register the ASP.NET Core MVC binder used by OpenIddict.
+                    // Note: if you don't call this method, you won't be able to
+                    // bind OpenIdConnectRequest or OpenIdConnectResponse parameters.
+                    options.UseMvc();
+
+                    // Enable the authorization and token endpoints (required to use the code flow).
+                    options.EnableAuthorizationEndpoint("/connect/authorize")
+                            .EnableTokenEndpoint("/connect/token");
+
+                    // Allow client applications to use the grant_type=password flow.
+                    options.AllowPasswordFlow();
+
+                    // During development, you can disable the HTTPS requirement.
+                    options.DisableHttpsRequirement();
+
+                    // Accept token requests that don't specify a client_id.
+                    options.AcceptAnonymousClients();
+                })
+
+                .AddValidation();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,6 +102,7 @@ namespace api
                 app.UseHsts();
             }
 
+            app.UseAuthentication();
             app.UseHttpsRedirection();
             app.UseMvc();
         }
