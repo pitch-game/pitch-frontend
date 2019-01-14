466baefd43da4d2e0c00246dd072a0a4fc72c461
Jacob Cann <j_cann@networkresearch.co.uk>
2019-01-14 13:15:08 +0000

    anime.js

--- frontend/src/app/pages/squad/squad.component.ts
+++ frontend/src/app/pages/squad/squad.component.ts
@@ -1,26 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from "@angular/core";
+import anime from 'animejs';
 
 @Component({
-  selector: 'app-squad',
-  templateUrl: './squad.component.html',
-  styleUrls: ['./squad.component.less']
+  selector: "app-squad",
+  templateUrl: "./squad.component.html",
+  styleUrls: ["./squad.component.less"]
 })
-export class SquadComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
+export class SquadComponent implements OnInit, AfterViewInit {
+  constructor() {}
+  ngOnInit() {}
 
   spins = {};
   players = {};
 
   spin(name) {
-    this.players[name] = {Name: 'Mbappé', Rating: 84, Position: 'ST'};
-    if (this.spins[name] === 'spin') {
+    this.players[name] = { Name: "Mbappé", Rating: 84, Position: "ST" };
+    if (this.spins[name] === "spin") {
       this.spins[name] = null;
     } else {
-      this.spins[name] = 'spin';
+      this.spins[name] = "spin";
     }
-  };
+  }
+
+  ngAfterViewInit(): void {
+    anime({
+      targets: [".player"],
+      rotateY: [{ value: 2520, duration: 4000 }],
+      duration: 3000,
+      loop: true,
+      easing: 'easeOutCubic'
+    }).add({ targets: '.player',  backgroundImage: 'none' }, 0);
+  }
+
+  onClick() {
+  }
 }
