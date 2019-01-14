63ff7d4bedf71ba5739c0fd64df5c44b142ee220
Jacob <jcbcn@users.noreply.github.com>
2019-01-14 21:03:51 +0000

    WIP player pack animation and styling

--- frontend/src/app/pages/squad/squad.component.ts
+++ frontend/src/app/pages/squad/squad.component.ts
@@ -10,26 +10,22 @@ export class SquadComponent implements OnInit, AfterViewInit {
   constructor() {}
   ngOnInit() {}
 
-  spins = {};
-  players = {};
-
-  spin(name) {
-    this.players[name] = { Name: "Mbappé", Rating: 84, Position: "ST" };
-    if (this.spins[name] === "spin") {
-      this.spins[name] = null;
-    } else {
-      this.spins[name] = "spin";
-    }
-  }
-
-  ngAfterViewInit(): void {
-    anime({
-      targets: [".player"],
-      rotateY: [{ value: 2520, duration: 4000 }],
-      duration: 3000,
-      loop: true,
-      easing: 'easeOutCubic'
-    }).add({ targets: '.player',  backgroundImage: 'none' }, 0);
+  click(): void {
+    anime.timeline({
+      loop: false
+    })
+    .add({ targets: ['.bow-left'],  translateY: [{ value: -250, duration: 500 }], opacity: [{ value: 0, duration: 250 }], easing: 'linear' }, 0)
+    .add({ targets: ['.bow-right'],  translateY: [{ value: -250, duration: 500 }], opacity: [{ value: 0, duration: 250 }], easing: 'linear' }, 0)
+    .add({ targets: ['.ribbon-left'],  translateX: [{ value: -250, duration: 500 }], opacity: [{ value: 0, duration: 1000 }], easing: 'linear' }, 100)
+    .add({ targets: ['.ribbon-right'],  translateX: [{ value: 250, duration: 500 }], opacity: [{ value: 0, duration: 1000 }], easing: 'linear' }, 100)
+    .add({ targets: ['.ribbon-top'],  translateY: [{ value: -250, duration: 750 }], opacity: [{ value: 0, duration: 750 }], easing: 'linear' }, 200)
+    .add({ targets: ['.ribbon-bottom'],  translateY: [{ value: 250, duration: 750 }], opacity: [{ value: 0, duration: 750 }], easing: 'linear' }, 200)
+    .add({ targets: ['.ribbon'],  opacity: [{ value: 0, duration: 1 }], easing: 'linear' }, 1000)
+    .add({ targets: ['.cover'],  opacity: [{ value: 0, duration: 3000 }], easing: 'linear' }, 1000)
+    .add({ targets: ['.player'], rotateY: [{ value: 2520, duration: 4000 }], easing: 'easeOutCubic'}, 500)
+    .add({ targets: ['.position'],  opacity: [{ value: 100, duration: 1000 }] }, '+=150')
+    .add({ targets: ['.rating'],  opacity: [{ value: 100, duration: 1000 }] }, '+=150')
+    .add({ targets: ['.name'],  opacity: [{ value: 100, duration: 1000 }] }, '+=500');
   }
 
   onClick() {
