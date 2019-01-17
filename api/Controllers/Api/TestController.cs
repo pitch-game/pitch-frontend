6fa4f80ce124dc16aabb8d1946008e5f143210a0
Jacob Cann <j_cann@networkresearch.co.uk>
2019-01-17 13:53:14 +0000

    Sample api test controller

---
+++ api/Controllers/Api/TestController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+[Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+[Route("api/[controller]")]
+[ApiController]
+public class TestController : ControllerBase {
+    [HttpGet]
+    public JsonResult GetTestItem(){
+        return new JsonResult("test");
+    }
+}
