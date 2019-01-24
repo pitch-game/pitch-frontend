3b724ba54638bf419138c1470c2423093aba8c85
Jacob <jcbcn@users.noreply.github.com>
2019-01-24 13:07:38 +0000

    HTTPS URLs on production

--- src/frontend/src/environments/environment.prod.ts
+++ src/frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiEndpoint: 'http://api.pitch-game.io',
-  appUri: 'http://pitch-game.io',
+  apiEndpoint: 'https://api.pitch-game.io',
+  appUri: 'https://pitch-game.io',
   version: 'X.X.X.X'
 };
