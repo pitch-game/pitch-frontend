597088e14aca646a83452fb20e8da42a200016ad
Jacob <jcbcn@users.noreply.github.com>
2019-06-17 21:43:10 +0100

    Polling for match and misc styling

--- src/app/pages/match/match.component.ts
+++ src/app/pages/match/match.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { ActivatedRoute } from '@angular/router';
+import { faSpinner, faCircle, faFutbol } from '@fortawesome/free-solid-svg-icons';
+import { timer, from, interval } from 'rxjs';
+import { concatMap, map, filter, take, flatMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-match',
@@ -11,16 +14,24 @@ import { ActivatedRoute } from '@angular/router';
 export class MatchComponent implements OnInit {
 
   match: any;
-  sessionId : string;
+  sessionId: string;
+  loadingIcon = faSpinner;
+  goalIcon = faFutbol;
   constructor(private httpClient: HttpClient, private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.route.paramMap.subscribe(params => {
       this.sessionId = params.get("id")
+      this.poll();
     })
-    this.httpClient.get(`${environment.apiEndpoint}/match/${this.sessionId}`).subscribe((result) => {
-      this.match = result;
-    });
+  }
+
+  poll() {
+    timer(0, 10000)
+      .pipe(flatMap(() => this.httpClient.get(`${environment.apiEndpoint}/match/${this.sessionId}`)))
+      .subscribe((result) => {
+        this.match = result;
+      });
   }
 
 }
