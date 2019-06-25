e5eed236b2c88dac3b775455959d721b55a0bf42
Jacob <jcbcn@users.noreply.github.com>
2019-06-25 12:14:34 +0100

    Update ng-tailwind.js

--- ng-tailwind.js
+++ ng-tailwind.js
@@ -1,8 +1,8 @@
 module.exports = {
   // Tailwind Paths
   configJS: 'tailwind.js',
-  sourceCSS: '.\\src\\tailwind.css',
-  outputCSS: '.\\src\\styles.css',
+  sourceCSS: 'src/tailwind.css',
+  outputCSS: 'src/styles.css',
   // Sass
   sass: false,
   // PurgeCSS Settings
