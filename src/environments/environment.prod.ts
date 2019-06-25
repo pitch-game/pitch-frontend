ccdcc715e2ccedb1b14eb505213d9a404654f004
Jacob <jcbcn@users.noreply.github.com>
2019-06-25 11:34:41 +0100

    Prod env settings and misc styling

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
-  apiEndpoint: 'http://api.pitch-game.io',
+  identityEndpoint: 'https://api.pitch-game.io/identity',
+  apiEndpoint: 'https://api.pitch-game.io',
   appUri: 'http://pitch-game.io',
   version: 'X.X.X.X'
 };
