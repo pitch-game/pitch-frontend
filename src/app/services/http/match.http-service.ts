81efa7a172969a1e396837398ad26550e474b0d6
Jacob <jcbcn@users.noreply.github.com>
2019-08-01 20:49:42 +0100

    Update match.http-service.ts

--- src/app/services/http/match.http-service.ts
+++ src/app/services/http/match.http-service.ts
@@ -11,28 +11,28 @@ export class MatchHttpService {
 
     constructor(private httpClient: HttpClient) { }
 
-    makeSub(off: string, on: string, matchId: string) {
-        return this.httpClient.post<any>(`${environment.apiEndpoint}/match/substitution`, { off, on, matchId }).toPromise();
-    }
-
     getWithQuery(query: CardQueryModel): Promise<MatchListItem[]> {
         var params = new HttpParams().set('skip', query.skip.toString()).set('take', query.take.toString());
         return this.httpClient.get<any[]>(`${environment.apiEndpoint}/match`, { params: params }).toPromise();
-    }
-
-    inProgress(): Promise<any> {
-        return this.httpClient.get<any>(`${environment.apiEndpoint}/match/status`).toPromise();
-    }
-
-    lineup(matchId: string): Promise<any> {
-        return this.httpClient.get<any>(`${environment.apiEndpoint}/match/lineup`, { params: { matchId } }).toPromise();
     }
 
     get(matchId: string) {
         return this.httpClient.get(`${environment.apiEndpoint}/match/${matchId}`);
     }
 
+    makeSub(off: string, on: string, matchId: string) {
+        return this.httpClient.post<any>(`${environment.apiEndpoint}/match/${matchId}/substitution`, { off, on }).toPromise();
+    }
+
+    lineup(matchId: string): Promise<any> {
+        return this.httpClient.get<any>(`${environment.apiEndpoint}/match/${matchId}/lineup`).toPromise();
+    }
+
+    inProgress(): Promise<any> {
+        return this.httpClient.get<any>(`${environment.apiEndpoint}/match/status`).toPromise();
+    }
+
     claim(): Promise<any> {
-        return this.httpClient.get<boolean>(`${environment.apiEndpoint}/match/claim`).toPromise(); //TODO this isn't doing what it was meant to
+        return this.httpClient.post<any>(`${environment.apiEndpoint}/match/claim`, null).toPromise();
     }
 }
