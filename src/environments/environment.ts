2e337b10603fbdcbedfac0dd38618fb48d467b17
jcbcn <jcbcn@users.noreply.github.com>
2020-03-14 22:17:55 +0000

    Local endpoints

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  identityEndpoint: 'https://api.pitch-game.io/identity',
-  apiEndpoint: 'https://api.pitch-game.io',
+  identityEndpoint: 'http://localhost.pitch-game.io/identity',
+  apiEndpoint: 'http://localhost.pitch-game.io',
   appUri: 'http://localhost:4200',
   version: '1.0.0.0'
 };
